e37d46a145d4ee20fe989d2efedc975ea30d57dd
Liming Xie <liming.xie@gmail.com>
2017-05-27 07:32:22 +0800

    port to mono/.NET 2.0

---
+++ Src/EngineIoClientDotNet.mono/Thread/EasyTimer_net20.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Threading;
+
+namespace Quobject.EngineIoClientDotNet.Thread
+{
+	public class EasyTimer
+	{
+		private Timer m_timer;
+
+		public EasyTimer(Action method, int period)
+		{
+			this.m_timer = new Timer(new TimerCallback(DoWork), method, 0, period);
+		}
+
+		static void DoWork(object obj)
+		{
+			Action method = (Action)obj;
+			if (method != null) method();
+		}
+
+		public static EasyTimer SetTimeout(Action method, int delayInMilliseconds)
+		{
+			// Returns a stop handle which can be used for stopping
+			// the timer, if required
+			return new EasyTimer(method, delayInMilliseconds);
+		}
+
+		public void Stop()
+		{
+			//var log = LogManager.GetLogger(Global.CallerName());
+			//log.Info("EasyTimer stop");
+			if (m_timer != null) m_timer.Dispose();
+		}
+
+		public static void TaskRun(Action action)
+		{
+			//ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), action);
+			var resetEvent = new ManualResetEvent(false);
+			ThreadPool.QueueUserWorkItem(
+			    arg => 
+			    {
+			        DoWork( action );
+					resetEvent.Set();
+			    });
+			resetEvent.WaitOne();
+		}
+
+		public static void TaskRunNoWait(Action action)
+		{
+			ThreadPool.QueueUserWorkItem(new WaitCallback(DoWork), action);
+		}
+	}
+
+
+}
